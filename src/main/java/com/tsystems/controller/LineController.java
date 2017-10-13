8171366dc63310660c8f4a7676240deec1c785e8
ixec2011 <ixec2011@gmail.com>
2017-10-13 11:14:26 +0300

    3.0

---
+++ src/main/java/com/tsystems/controller/LineController.java
@@ -0,0 +1,37 @@
+package com.tsystems.controller;
+
+import com.tsystems.service.impl.LineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LineController {
+
+    @Autowired
+    private LineService lineService;
+
+    @PostMapping(path = "/line/add")
+    public ModelAndView addLine(String line) {
+        lineService.addLine(line);
+        ModelAndView modelAndView = new ModelAndView("list_line");
+        modelAndView.addObject("lineList", lineService.getListStations());
+        return modelAndView;
+    }
+
+    @PostMapping(path = "/line/new")
+    public ModelAndView newLine() {
+        return new ModelAndView("add_line");
+    }
+
+    @PostMapping(path = "/line/list")
+    public ModelAndView listLine() {
+        ModelAndView modelAndView = new ModelAndView("list_line");
+        modelAndView.addObject("lineList", lineService.getListStations());
+        return modelAndView;
+    }
+
+}
+
+
